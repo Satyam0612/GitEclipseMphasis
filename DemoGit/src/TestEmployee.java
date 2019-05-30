be38c93ba3b6280e4364eb11b5e1bf2a98129093
pemabhutia14 <pemabhutia14@gmail.com>
2019-05-30 16:53:37 +0530

    TestEmployeeyoyo

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,12 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1=new Employee();
+		e1.setEmpId(101);
+		e1.setEmpName("Ramesh");
+		e1.setEmpSal("1000");
+	}
+
+}
